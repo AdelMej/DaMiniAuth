e08b2ba03e8929a70d85c5a4cad2b858592e1d85
Adel <adelmejrissi@gmail.com>
2025-11-11 19:13:47 +0100

    initial spring boot commit

---
+++ DaMiniAuthAPI/src/main/java/com/damini/authapi/DaMiniAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.damini.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaMiniAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaMiniAuthApiApplication.class, args);
+	}
+
+}
---
+++ DaMiniAuthAPI/src/test/java/com/damini/authapi/DaMiniAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damini.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaMiniAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
